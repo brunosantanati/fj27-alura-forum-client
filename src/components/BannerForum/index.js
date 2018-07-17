4a6b3e8216d70765b1ebca905f149fbcf1185591
thaiswork <thais.work17@gmail.com>
2018-07-17 18:18:12 -0300

    Adicionando banner

---
+++ src/components/BannerForum/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BannerForum extends Component {
+    render() {
+        return (
+            <div>
+                <section className="banner">
+                    <div className="container">
+                        <h1 className="banner-title">
+                            <a href="https://cursos.alura.com.br/forum" className="banner-title-link">Fórum </a>
+                        </h1>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+} export default BannerForum;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './style.css';
 
 import HeaderForum from '../../components/HeaderForum';
-
+import BannerForum from '../../components/BannerForum';
 class HomePage extends Component {
   
   render() {
     return (
       <div>
         <HeaderForum />
+        <BannerForum />
       </div>
     );
   }
