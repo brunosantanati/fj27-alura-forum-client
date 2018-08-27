60af73dfef1124d4bc7580a37842c46494549065
Rafael Rollo <rafaelrollo92@gmail.com>
2018-08-27 17:14:06 -0300

    Fazendo requests em função da filtragem do usuario

--- src/components/MenuDuvidasForum/index.js
+++ src/components/MenuDuvidasForum/index.js
@@ -6,6 +6,7 @@ class MenuDuvidasForum extends Component {
     constructor() {
         super();
         this.state = {
+            filtroAtivo: '',
             categoriaAtiva: CategoriasDoForum['TODAS'],
             seletorDeCatoriaAtivo: false
         }
@@ -14,16 +15,23 @@ class MenuDuvidasForum extends Component {
     toggleDropDownCategoria = () => {
         this.setState({seletorDeCatoriaAtivo: !this.state.seletorDeCatoriaAtivo})
     }
-    
+
     selecionaCategoria = (categoria) => {
         this.setState({categoriaAtiva: CategoriasDoForum[categoria]})
+        setTimeout(() => this.buscaDuvidas(), 300);
     }
 
-    buscaDuvidas = (recurso) => {
-        var { atualizaDuvidasCallback } = this.props;
-    
-        const uri = `http://localhost:8080/${recurso}`;
-    
+    toggleRestricoes = (filtro) => {
+        this.setState({filtroAtivo: filtro})
+        setTimeout(() => this.buscaDuvidas(), 300);
+    }
+
+    buscaDuvidas = () => {
+        const { atualizaDuvidasCallback } = this.props;
+        const { filtroAtivo, categoriaAtiva } = this.state;
+        
+        const uri = this.resolveURI(filtroAtivo, categoriaAtiva);
+        
         fetch(uri)
             .then(response => {
                 if(response.ok)
@@ -35,6 +43,19 @@ class MenuDuvidasForum extends Component {
                 atualizaDuvidasCallback(json);
             })
             .catch(e => alert(e.message));
+    }
+
+    resolveURI(filtro, categoria) {
+        let uri = `http://localhost:8080/api/topics/`;
+
+        if (filtro !== '')
+            uri += filtro;
+            
+        if (JSON.stringify(categoria) !== JSON.stringify(CategoriasDoForum['TODAS']))
+            uri += `?categoryName=${categoria.nome}`
+
+        console.log(uri)    
+        return uri;
     }
 
     render() {
@@ -77,7 +98,7 @@ class MenuDuvidasForum extends Component {
                                             <span onClick={() => this.selecionaCategoria('INFRA')}>Infraestrutura</span>
                                         </li>
                                         <li className="menuDesign" data-option="" data-value="design-ux">
-                                            <span onClick={() => this.selecionaCategoria('DESIGN')}>Design &amp; UX</span>
+                                            <span onClick={() => this.selecionaCategoria('DESIGN')}>Design</span>
                                         </li>
                                         <li className="menuBusiness" data-option="" data-value="business">
                                             <span onClick={() => this.selecionaCategoria('BUSINESS')}>Business</span>
@@ -88,18 +109,18 @@ class MenuDuvidasForum extends Component {
                             </fieldset>
 
                             <fieldset className="restrictions">
-                                <input className="restrictionsInput--all" type="radio"  name="restriction" value="todos" id="todos" onChange={() => this.buscaDuvidas('api/topics')}/>
-                                <label className="restrictionsLabel restrictionsLabel--all" htmlFor="todos">
+                                <input className="restrictionsInput--all" type="radio"  name="restriction" value="todos" id="todos"/>
+                                <label className="restrictionsLabel restrictionsLabel--all" htmlFor="todos" onClick={() => this.toggleRestricoes('')}>
                                     Todos
                                 </label>
 
-                                <input className="restrictionsInput--all" type="radio" name="restriction" value="sem-resposta" id="sem-resposta" onChange={() => this.buscaDuvidas('api/topics/not-answered')} />
-                                <label className="restrictionsLabel restrictionsLabel--all" htmlFor="sem-resposta">
+                                <input className="restrictionsInput--all" type="radio" name="restriction" value="sem-resposta" id="sem-resposta"/>
+                                <label className="restrictionsLabel restrictionsLabel--all" htmlFor="sem-resposta" onClick={() => this.toggleRestricoes('not-answered')}>
                                     Sem resposta
                                 </label>
 
-                                <input className="restrictionsInput--all" type="radio" name="restriction" value="sem-solucao" id="sem-solucao" onChange={() => this.buscaDuvidas('api/topics/not-solved')}/>
-                                <label className="restrictionsLabel restrictionsLabel--all" htmlFor="sem-solucao">
+                                <input className="restrictionsInput--all" type="radio" name="restriction" value="sem-solucao" id="sem-solucao"/>
+                                <label className="restrictionsLabel restrictionsLabel--all" htmlFor="sem-solucao" onClick={() => this.toggleRestricoes('not-solved')}>
                                     Sem solução
                                 </label>
                             </fieldset>
@@ -116,11 +137,32 @@ export default MenuDuvidasForum;
 
 
 const CategoriasDoForum = {
-    'TODAS': {nome: 'Todas as categorias', menuClass: 'menuTodasCategorias'},
-    'MOBILE': {nome: 'Mobile', menuClass: 'menuMobile'},
-    'PROGRAMACAO': {nome: 'Programação', menuClass: 'menuProgramacao'},
-    'FRONT': {nome: 'Front-end', menuClass: 'menuFrontEnd'},
-    'INFRA': {nome: 'Infraestrutura', menuClass: 'menuInfraestrutura'},
-    'DESIGN': {nome: 'Design & UX', menuClass: 'menuDesign'},
-    'BUSINESS': {nome: 'Business', menuClass: 'menuBusiness'}
+    'TODAS': {
+        nome: 'Todas as categorias', 
+        menuClass: 'menuTodasCategorias'
+    },
+    'MOBILE': {
+        nome: 'Mobile', 
+        menuClass: 'menuMobile'
+    },
+    'PROGRAMACAO': {
+        nome: 'Programação', 
+        menuClass: 'menuProgramacao'
+    },
+    'FRONT': {
+        nome: 'Front-end', 
+        menuClass: 'menuFrontEnd'
+    },
+    'INFRA': {
+        nome: 'Infraestrutura', 
+        menuClass: 'menuInfraestrutura'
+    },
+    'DESIGN': {
+        nome: 'Design', 
+        menuClass: 'menuDesign'
+    },
+    'BUSINESS': {
+        nome: 'Business', 
+        menuClass: 'menuBusiness'
+    }
 };
